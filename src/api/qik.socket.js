ac1eb27e5dffc3981f5d86e9aeace6dbda92e45c
Cade Embery <s-cade@subsplash.com>
2022-08-24 21:51:55 +1000

    socket updates

--- src/api/qik.socket.js
+++ src/api/qik.socket.js
@@ -1,9 +1,10 @@
-
 import { EventDispatcher } from './qik.utils.js';
 
 ///////////////////////////////////////////////////
 
-var QikSocket = function(qik, mode) {
+
+
+const QikSocket = function(qik, mode) {
 
     mode = mode || 'production';
 
@@ -14,27 +15,99 @@ var QikSocket = function(qik, mode) {
     ///////////////////////////////////////////////////
 
     var service = {
-        debug:false,
+        debug: false,
         url: `wss://iqtm6zjz3l.execute-api.ap-southeast-2.amazonaws.com/${mode}`,
+        connected:false,
     }
 
     var socket;
+    var timer;
+    var buckets = {};
 
-  
+
     //Create a new dispatcher
     var dispatcher = new EventDispatcher();
     dispatcher.bootstrap(service);
 
     ///////////////////////////////////////////////////
 
+    function getSubscriptions() {
+        return Object.keys(buckets);
+    }    
+
+    ///////////////////////////////////////////////////
+
+    service.channel = async function(key) {
+        if(buckets[key]) {
+            return buckets[key];
+        }
+
+        const bucket = new EventDispatcher();
+        buckets[key] = bucket;
+
+        if(!service.connected) {
+            // Return the socket
+            await service.connect();
+        }
+        
+        console.log('subscribe', key, service.connected);
+        // Subscribe to this channel
+        service.subscribe(key);
+
+        // Listen for all relevant events
+        service.addEventListener(key, dispatchMessage);
+
+        function dispatchMessage(message) {
+            console.log('Dispatch a message now', key, message)
+            bucket.dispatch('message', message);
+        }
+
+        // Destroy the service
+        bucket.destroy = function() {
+            service.removeEventListener(key, dispatchMessage);
+            buckets[key] = null;
+            console.log('destroyed', key);
+        }
+
+        return buckets[key];
+    }
+
+    ///////////////////////////////////////////////////
+
+    function ping() {
+        console.log('ping');
+        broadcast({
+            action: 'ping',
+        })
+    }
+
+    function startHeartbeat() {
+        if(timer) {
+            return;
+        }
+        timer = setInterval(ping, 10000);
+    }
+
+    function stopHeartbeat() {
+        if(!timer) {
+            return;
+        }
+
+        clearInterval(timer)
+        timer = null;
+    }
+
+    ///////////////////////////////////////////////////
+
     function socketOpened(event) {
-        console.log('[event] socketOpened', event);
         service.debug ? console.log("[socket] Connection open") : null;
+        service.connected = true;
         dispatcher.dispatch('connected', event);
+        
+        // startHeartbeat();
     }
 
     function socketClosed(event) {
-        console.log('[event] socketClosed', event);
         if (event.wasClean) {
             service.debug ? console.log(`[socket] Connection closed cleanly, code=${event.code} reason=${event.reason}`) : null;
         } else {
@@ -43,8 +116,10 @@ var QikSocket = function(qik, mode) {
             service.debug ? console.log('[socket] - connection closed due to error', event) : null
         }
 
+        service.connected = false;
         dispatcher.dispatch('disconnected', event);
         socket = null;
+        // stopHeartbeat();
     }
 
     function socketError(error) {
@@ -54,34 +129,55 @@ var QikSocket = function(qik, mode) {
     }
 
     function socketMessageReceived(event) {
-         console.log('[event] socketMessage', event);
         const eventData = JSON.parse(event.data);
         service.debug ? console.log("[socket] message received", eventData) : null;
-        
+
         // Dispatch a generic message
         dispatcher.dispatch('message', eventData);
 
-        if(eventData.channel) {
+        if (eventData.channel) {
             dispatcher.dispatch(eventData.channel, eventData);
         }
     }
 
-    function broadcast(data) {
+    async function broadcast(data) {
         socket.send(JSON.stringify(data));
     }
 
+    ///////////////////////////////////////////////////
+
+    function wait() {
+        return new Promise(function(resolve) {
+            function check() {
+                if(service.connected) {
+                     console.log('connected successfully')
+                    return resolve(socket);
+                }
+
+                console.log('check in a second')
+                setTimeout(check, 1000);
+            }
+
+            check();
+        })
+    }
 
     ///////////////////////////////////////////////////
 
     service.connect = async function() {
-        if(socket) {
-            service.debug ? console.log('[socket] - Socket is already connected') : null;
-            return;
+        if (socket) {
+
+            if(!socket.connected) {
+                return wait();
+            }
+
+            console.log('[socket] - Socket is already connected');
+            return socket;
         }
 
         const accessToken = qik.auth.getCurrentToken();
 
-        if(!accessToken) {
+        if (!accessToken) {
             service.debug ? console.log('[socket] - Must be authenticated to connect to socket') : null;
             return;
         }
@@ -91,15 +187,16 @@ var QikSocket = function(qik, mode) {
         socket.addEventListener('error', socketError);
         socket.addEventListener('open', socketOpened);
         socket.addEventListener('message', socketMessageReceived);
-
         service.debug ? console.log('[socket] - Connected with event listeners') : null;
+
+        return wait();
     }
 
     ///////////////////////////////////////////////////
 
     service.close = async function() {
-        if(!socket) {
-            service.debug ? console.log('[socket] - Socket is not connected') : null;
+        if (!socket) {
+            console.log('[socket] - Socket is not connected');
             return;
         }
 
@@ -109,40 +206,42 @@ var QikSocket = function(qik, mode) {
         socket.removeEventListener('open', socketOpened);
         socket.removeEventListener('message', socketMessageReceived);
         socket = null;
-        socketClosed();
+        socketClosed({ wasClean: true });
     }
 
-    service.disconnect = service.close();
+    service.disconnect = service.close;
 
     ///////////////////////////////////////////////////
 
     service.subscribe = async function(channel) {
-        if(!socket) {
-            service.debug ? console.log(`[socket] - Can't subscribe to channel as socket is not connected`)  : null;
+        if (!socket) {
+            console.log(`[socket] - Can't subscribe to channel as socket is not connected`);
             return;
         }
 
         broadcast({
-            action:'subscribe',
+            action: 'subscribe',
             channel,
         })
 
-        service.debug ? console.log(`[socket] - subscribed to ${channel}`)  : null;
+        dispatcher.dispatch('subscribe', channel)
+        service.debug ? console.log(`[socket] - subscribed to ${channel}`) : null;
     }
 
     ///////////////////////////////////////////////////
 
     service.unsubscribe = async function(channel) {
-        if(!socket) {
-            service.debug ? console.log(`[socket] - Can't unsubscribe from channel as socket is not connected`)  : null;
+        if (!socket) {
+            console.log(`[socket] - Can't unsubscribe from channel as socket is not connected`);
             return;
         }
 
         broadcast({
-            action:'unsubscribe',
+            action: 'unsubscribe',
             channel,
         })
 
+        dispatcher.dispatch('unsubscribe', channel)
         service.debug ? console.log(`[socket] - unsubscribed from ${channel}`) : null;
     }
 
